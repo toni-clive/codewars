d937c0d82ec5540a862c36594af41811937f46a8
clive_mb_air <clivetoniato1@hotmail.com>
2026-05-25 18:46:08 +0100

    nameToMatrix completed using Array.from

---
+++ 6_Kuy/nameToMatrix.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5a91e0793e9156ccb0003f6e/train/javascript
+
+function matrixfy(str) {
+  if(!str) return 'name must be at least one letter'
+  const size=Math.ceil(Math.sqrt(str.length))
+  let letterIndex = 0
+  let matrix=Array.from({length:size},(_,i)=>Array.from({length:size},(_,j)=>  str[j+(i*size)]||'.' ))
+  return matrix
+}
+
+console.log(matrixfy('Beyonce'), [['B', 'e', 'y'], ['o', 'n', 'c'], ['e', '.', '.']])
