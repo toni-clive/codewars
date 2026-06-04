64a074dc59ed051935a0ed7fff2268a86deecfba
clive_mb_air <clivetoniato@googlemail.com>
2026-06-04 13:11:06 +0100

    isLeapYear solved using rules for leapYear

---
+++ js/7_Kuy/isLeapYear.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/526c7363236867513f0005ca/train/javascript
+// Tags 
+// Date Time
+// Algorithms
+function isLeapYear(year) {
+  // TODO
+  return (year%4==0 && year%100!=0) || year%400==0
+}
