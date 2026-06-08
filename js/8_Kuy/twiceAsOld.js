e99017be4fff71d6c040f1e0c3492a2bb9940d9c
clive_mb_air <clivetoniato@googlemail.com>
2026-06-08 11:08:34 +0100

    twiceAsOld solved using formula D + T = 2(S + T)

---
+++ js/8_Kuy/twiceAsOld.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+// Tags
+// Fundamentals
+// Mathematics
+
+// Formula D + T = 2(S + T)
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2*sonYearsOld)
+}
