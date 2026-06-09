9ec813086650120e4e3d254851a6a32df79a1485
clive_mb_air <clivetoniato@googlemail.com>
2026-06-09 14:51:56 +0100

    caffeineBuzz updated using LCD

--- js/7_Kuy/caffeineBuzz.js
+++ js/7_Kuy/caffeineBuzz.js
@@ -1,5 +1,7 @@
 // https://www.codewars.com/kata/5434283682b0fdb0420000e6/train/javascript
-
+// Tags 
+// Fundamentals
+// Strings
 
 function caffeineBuzz(n) {
 let res = ''
@@ -7,6 +9,13 @@ let res = ''
   else if(n%3==0) res+= "Java"
    if((n%3 ==0 || n%4==0) && n%2==0) res+= "Script"
   return res|| 'mocha_missing!'
+}
+
+function caffeineBuzz(n) {
+  if(n%3==0 && n%4==0) return "CoffeeScript"
+  if(n%6==0) return "JavaScript"
+  if(n%3==0) return "Java"
+    return 'mocha_missing!';
 }
 
 
