a4ee0233db029ca7f48e69e903c1022d7366d28b
clive_mb_air <clivetoniato@googlemail.com>
2026-06-10 10:13:29 +0100

    distanceBetweenPoints completed using distance formula

---
+++ js/8_Kuy/distanceBetweenPoints.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/58dced7b702b805b200000be/train/javascript
+
+// Tags 
+// Geometry
+// Fundamentals
+//  Euclidean distance formula
+
+function distanceBetweenPoints(a, b) {
+  return Math.sqrt((b.y-a.y)**2+(b.x-a.x)**2); // your code here
+}
